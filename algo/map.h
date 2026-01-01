977358bcaf44836ca7339015667ae3125eb6fe6d
Fikret Güney Ersezer <84100145+Fikoko@users.noreply.github.com>
2026-01-01 22:18:14 +0300

    Implement MAP_TYPED and MAP_VEC macros
    Added typed macros for mapping arrays and vectors.

--- algo/map.h
+++ algo/map.h
@@ -17,7 +17,7 @@
 */
 
 /*
-    Map function:
+    Map function (generic):
     - in  : pointer to input element
     - out : pointer to output element (must be writable)
 */
@@ -25,7 +25,6 @@ typedef void (*MapFn)(void *out, const void *in);
 
 /*
     Maps input items into output items.
-
     Requirements:
     - input  : array of pointers to input elements
     - output : array of pointers to writable output storage
@@ -44,5 +43,40 @@ static inline void map(
         f(output[i], input[i]);
     }
 }
+
+/* ============================================================
+   Typed macro (for arrays or Vec_<type>)
+   ============================================================ */
+
+/*
+    MAP_TYPED(out, in, len, Type, fn)
+
+    - out  : pointer to output array or Vec_<type>.items
+    - in   : pointer to input array or Vec_<type>.items
+    - len  : number of elements
+    - Type : element type
+    - fn   : function of signature `void fn(Type *out, const Type *in)`
+*/
+#define MAP_TYPED(out, in, len, Type, fn) \
+    do { \
+        if ((out) && (in) && (fn)) { \
+            for (size_t i = 0; i < (len); i++) { \
+                fn(&(out)[i], &(in)[i]); \
+            } \
+        } \
+    } while(0)
+
+/*
+    MAP_VEC(out_vec, in_vec, Type, fn)
+
+    Convenience macro for Vec_<type>:
+
+    - out_vec : destination Vec_<type>
+    - in_vec  : source Vec_<type>
+    - Type    : element type
+    - fn      : transformation function
+*/
+#define MAP_VEC(out_vec, in_vec, Type, fn) \
+    MAP_TYPED((out_vec).items, (in_vec).items, ((out_vec).len < (in_vec).len ? (out_vec).len : (in_vec).len), Type, fn)
 
 #endif /* CANON_C_ALGO_MAP_H */
